93cfae05414ce9c6ab3ae44e19b78d047471328f
Lucas Lourenção <lucas.l.lourencao@gmail.com>
2023-03-13 17:02:19 -0300

    mod-aaliacoes: resolucao da atividade do mod 01

---
+++ mentorama-python-avancado/mod-avaliacoes/av-mod-01/informaNumero.py
@@ -0,0 +1,15 @@
+# import pytest
+# # Importação das funções:
+# from verificaMultiplo import verficaMultiplo_5
+
+
+# class verificaNumero(self):
+#     def setup(self):
+#         pass
+
+#     # def testeMultiplo_5(self):
+#     #     # variavel para recebimento do numero a ser testado:
+#     #     recebeNumero = input('Informe um número: ')
+#     #     resultado = verficaMultiplo_5(recebeNumero)
+
+#     #     # assert resultado == f'${num} é múltiplo de 5!'
---
+++ mentorama-python-avancado/mod-avaliacoes/av-mod-01/verificaMultiplo.py
@@ -0,0 +1,5 @@
+
+# Função para verificar se o número é multiplo de 5:
+def verficaMultiplo_5(num):
+    if num % 5 == 0:
+        return f'${num} é múltiplo de 5!'
