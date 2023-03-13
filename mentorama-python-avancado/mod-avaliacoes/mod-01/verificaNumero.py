da9fcde32ab5d00fdda8d3a9a7bb4dede637a468
Lucas Lourenção <lucas.l.lourencao@gmail.com>
2023-03-13 06:29:14 -0300

    Iniciando o exercício avaliativo do módulo 01

---
+++ mentorama-python-avancado/mod-avaliacoes/mod-01/verificaNumero.py
@@ -0,0 +1,14 @@
+# Módulo com funções que serão importadas
+
+def verificaMultiplo_5(numeroNatural):
+    if numeroNatural % 5 == 0:
+        return 'fizz'
+
+# def verificaMultiplo_7(numeroNatural):
+#     if numeroNatural % 7 == 0:
+#         return 'fizz'
+
+# def verificaMultiplo_5e7(numeroNatural):
+#     if numeroNatural % 5 == 0 & numeroNatural % 7 == 0:
+#         return 'fizzbuzz'
+    
