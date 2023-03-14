a74665e686705409535bb165afd3c42c442043ed
Lucas Lourenção <lucas.l.lourencao@gmail.com>
2023-03-14 14:04:46 -0300

    mod-avaliacoes: mod-01 - trabalhando na atividade 01

--- mentorama_python_avancado/mod_avaliacoes/mod_01/verificaNumero.py
+++ mentorama_python_avancado/mod_avaliacoes/mod_01/verificaNumero.py
@@ -3,45 +3,46 @@
 # Função que verifica se o número informado é múltiplo de 5 e satisfaz as demais condições:
 def verificaMultiplo_5():
     try:
-        numeroNatural = int(input('Informe um número natural: ')) 
+        numeroNatural = int(input('Informe um número natural: '))
         if numeroNatural % 5 == 0 and numeroNatural >= 0:
             resultado = print('fizz')
         else:
             resultado = print('O número informado não é múltiplo de 5!')
-    except:  
+    except:
         resultado = 'Você precisa informar um número natural, ou seja, inteiro e positivo!'
-    
-   
+
     return resultado
 
 # Função que verifica se o número informado é múltiplo de 7 e satisfaz as demais condições:
+
+
 def verificaMultiplo_7():
     try:
-        numeroNatural = int(input('Informe um número natural: ')) 
+        numeroNatural = int(input('Informe um número natural: '))
         if numeroNatural % 7 == 0 and numeroNatural >= 0:
             resultado = print('buzz')
         else:
             resultado = print('O número informado não é múltiplo de 7!')
-    except:  
+    except:
         resultado = 'Você precisa informar um número natural, ou seja, inteiro e positivo!'
-    
-   
+
     return resultado
 
 # Função que verifica se o número informado é múltiplo de 5, 7 e satisfaz as demais condições:
+
+
 def verificaM5_M7():
     try:
-        numeroNatural = int(input('Informe um número natural: ')) 
+        numeroNatural = int(input('Informe um número natural: '))
         if numeroNatural % 5 == 0 and numeroNatural % 7 == 0 and numeroNatural >= 0:
             resultado = print('fizzbuzz')
         else:
             resultado = print('miss')
-    except:  
+    except:
         resultado = 'Você precisa informar um número natural, ou seja, inteiro e positivo!'
-    
-   
+
     return resultado
-    
+
 
 # def verificaMultiplo_7(numeroNatural):
 #     if numeroNatural % 7 == 0:
@@ -50,4 +51,3 @@ def verificaM5_M7():
 # def verificaMultiplo_5e7(numeroNatural):
 #     if numeroNatural % 5 == 0 & numeroNatural % 7 == 0:
 #         return 'fizzbuzz'
-    
